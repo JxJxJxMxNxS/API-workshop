be2a2d4515687df51a5095be8c57c986818ae99a
Juan Juarez <jjuarez@skytouchtechnology.com>
2024-10-31 03:04:46 -0600

    workshop: add sql file to populate todos

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO to_do (description,status,priority) VALUES ('Study for certification','pending',1);
+INSERT INTO to_do (description,status,priority) VALUES ('Work on availability bug','in progress',3);
+INSERT INTO to_do (description,status,priority) VALUES ('Check guest data bug','pending',2);
+INSERT INTO to_do (description,status,priority) VALUES ('Make slides for spring workshop','in progress',2);
